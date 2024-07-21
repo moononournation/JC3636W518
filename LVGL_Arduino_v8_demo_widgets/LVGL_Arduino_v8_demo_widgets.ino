ae8fd7e3ec7e3f9d4179f8e7f9000693c181e5c3
moononournation <moononournation@gmail.com>
2024-07-21 14:16:41 +0800

    first commit

---
+++ LVGL_Arduino_v8_demo_widgets/LVGL_Arduino_v8_demo_widgets.ino
@@ -0,0 +1,203 @@
+/*Using LVGL with Arduino requires some extra steps:
+ *Be sure to read the docs here: https://docs.lvgl.io/master/get-started/platforms/arduino.html  */
+
+#include <lvgl.h>
+
+/*To use the built-in examples and demos of LVGL uncomment the includes below respectively.
+ *You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
+ Note that the `lv_examples` library is for LVGL v7 and you shouldn't install it for this version (since LVGL v8)
+ as the examples and demos are now part of the main LVGL library. */
+
+// #include <examples/lv_examples.h>
+#include <demos/lv_demos.h>
+
+#define DIRECT_MODE // Uncomment to enable full frame buffer
+
+#include "JC3636W518.h"
+
+#include "touch.h"
+
+uint32_t screenWidth;
+uint32_t screenHeight;
+uint32_t bufSize;
+lv_disp_draw_buf_t draw_buf;
+lv_color_t *disp_draw_buf;
+lv_disp_drv_t disp_drv;
+
+#if LV_USE_LOG != 0
+/* Serial debugging */
+void my_print(const char *buf)
+{
+  Serial.printf(buf);
+  Serial.flush();
+}
+#endif
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p)
+{
+#ifndef DIRECT_MODE
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+#if (LV_COLOR_16_SWAP != 0)
+  gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#else
+  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#endif
+#endif // #ifndef DIRECT_MODE
+
+  lv_disp_flush_ready(disp_drv);
+}
+
+/*Read the touchpad*/
+void my_touchpad_read(lv_indev_drv_t *indev_drv, lv_indev_data_t *data)
+{
+  if (touch_has_signal())
+  {
+    if (touch_touched())
+    {
+      data->state = LV_INDEV_STATE_PR;
+
+      /*Set the coordinates*/
+      data->point.x = touch_last_x;
+      data->point.y = touch_last_y;
+    }
+    else if (touch_released())
+    {
+      data->state = LV_INDEV_STATE_REL;
+    }
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_REL;
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+  // while(!Serial);
+  Serial.println("Arduino_GFX LVGL_Arduino_v8 example ");
+  String LVGL_Arduino = String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+  Serial.println(LVGL_Arduino);
+
+#ifdef GFX_EXTRA_PRE_INIT
+  GFX_EXTRA_PRE_INIT();
+#endif
+
+  // Init Display
+  if (!gfx->begin())
+  {
+    Serial.println("gfx->begin() failed!");
+  }
+  gfx->fillScreen(BLACK);
+
+#ifdef GFX_BL
+  ledcSetup(1, 12000, 8);   // 12 kHz PWM, 8-bit resolution
+  ledcAttachPin(GFX_BL, 1); // assign RGB led pins to channels
+  ledcWrite(1, 63);
+#endif
+
+  // Init touch device
+  touch_init(gfx->width(), gfx->height(), gfx->getRotation());
+
+  lv_init();
+
+#if LV_USE_LOG != 0
+  lv_log_register_print_cb(my_print); /* register print function for debugging */
+#endif
+
+  screenWidth = gfx->width();
+  screenHeight = gfx->height();
+
+#ifdef DIRECT_MODE
+  bufSize = screenWidth * screenHeight;
+#else
+  bufSize = screenWidth * 40;
+#endif
+
+#ifdef ESP32
+#if defined(DIRECT_MODE) && (defined(CANVAS) || defined(RGB_PANEL))
+  disp_draw_buf = (lv_color_t *)gfx->getFramebuffer();
+#else  // !(defined(DIRECT_MODE) && (defined(CANVAS) || defined(RGB_PANEL)))
+  disp_draw_buf = (lv_color_t *)heap_caps_malloc(bufSize * 2, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+  if (!disp_draw_buf)
+  {
+    // remove MALLOC_CAP_INTERNAL flag try again
+    disp_draw_buf = (lv_color_t *)heap_caps_malloc(bufSize * 2, MALLOC_CAP_8BIT);
+  }
+#endif // !(defined(DIRECT_MODE) && (defined(CANVAS) || defined(RGB_PANEL)))
+#else // !ESP32
+  Serial.println("LVGL disp_draw_buf heap_caps_malloc failed! malloc again...");
+  disp_draw_buf = (lv_color_t *)malloc(bufSize * 2);
+#endif // !ESP32
+  if (!disp_draw_buf)
+  {
+    Serial.println("LVGL disp_draw_buf allocate failed!");
+  }
+  else
+  {
+    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, bufSize);
+
+    /* Initialize the display */
+    lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+#ifdef DIRECT_MODE
+    disp_drv.direct_mode = true;
+#endif
+    lv_disp_drv_register(&disp_drv);
+
+    /* Initialize the (dummy) input device driver */
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register(&indev_drv);
+
+    /* Option 1: Create simple label */
+    // lv_obj_t *label = lv_label_create(lv_scr_act());
+    // lv_label_set_text(label, "Hello Arduino! (V" GFX_STR(LVGL_VERSION_MAJOR) "." GFX_STR(LVGL_VERSION_MINOR) "." GFX_STR(LVGL_VERSION_PATCH) ")");
+    // lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+
+    /* Option 2: Try an example. See all the examples
+     * online: https://docs.lvgl.io/master/examples.html
+     * source codes: https://github.com/lvgl/lvgl/tree/e7f88efa5853128bf871dde335c0ca8da9eb7731/examples */
+    // lv_example_btn_1();
+
+    /* Option 3: Or try out a demo. Don't forget to enable the demos in lv_conf.h. E.g. LV_USE_DEMOS_WIDGETS*/
+    lv_demo_widgets();
+    // lv_demo_benchmark();
+    // lv_demo_keypad_encoder();
+    // lv_demo_music();
+    // lv_demo_stress();
+  }
+
+  Serial.println("Setup done");
+}
+
+void loop()
+{
+  lv_timer_handler(); /* let the GUI do its work */
+
+#ifdef DIRECT_MODE
+#if defined(CANVAS) || defined(RGB_PANEL)
+  gfx->flush();
+#else // !(defined(CANVAS) || defined(RGB_PANEL))
+#if (LV_COLOR_16_SWAP != 0)
+  gfx->draw16bitBeRGBBitmap(0, 0, (uint16_t *)disp_draw_buf, screenWidth, screenHeight);
+#else
+  gfx->draw16bitRGBBitmap(0, 0, (uint16_t *)disp_draw_buf, screenWidth, screenHeight);
+#endif
+#endif // !(defined(CANVAS) || defined(RGB_PANEL))
+#else  // !DIRECT_MODE
+#ifdef CANVAS
+  gfx->flush();
+#endif
+#endif // !DIRECT_MODE
+}
