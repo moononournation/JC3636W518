c5cb6de3a4ace785ac44572f274647810fc8baac
moononournation <moononournation@gmail.com>
2024-07-21 22:27:42 +0800

    image source

--- ImgViewerMjpeg_Zoomquilt/ImgViewerMjpeg_Zoomquilt.ino
+++ ImgViewerMjpeg_Zoomquilt/ImgViewerMjpeg_Zoomquilt.ino
@@ -1,9 +1,8 @@
 /*******************************************************************************
  * Motion JPEG Image Viewer
  * This is a simple Motion JPEG image viewer example
- * Image Source: https://www.pexels.com/video/earth-rotating-video-856356/
- * cropped: x: 598 y: 178 width: 720 height: 720 resized: 352x352
- * ffmpeg -i "Pexels Videos 3931.mp4" -ss 0 -t 20.4s -vf "reverse,setpts=0.5*PTS,fps=10,vflip,hflip,rotate=90,crop=720:720:178:598,scale=352:352:flags=lanczos" -q:v 9 earth352.mjpeg
+ * Image Source: https://youtu.be/RpHnKaxt_OQ
+ * ffmpeg -y -i "The Zoomquilt - an infinitely zooming collaborative painting.mp4" -ss 0 -t 00:02:00.000 -vf "fps=5,scale=-1:352:flags=lanczos,crop=352:352:(in_w-352)/2:0" -q:v 7 zoomquilt.mjpeg
  *
  * Dependent libraries:
  * JPEGDEC: https://github.com/bitbank2/JPEGDEC.git
